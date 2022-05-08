4726bad1b5439335acf2a4e0bc5b6c416dc55c06
BrazhnikDA <M1ND228@yandex.ru>
2022-05-08 11:38:22 +0300

    Solution task3 tbb

---
+++ modules/task_3/brazhnik_d_sort_shell_simple_merge_tbb/shell_sort_tbb.h
@@ -0,0 +1,16 @@
+// Copyright 2022 Brazhnik Dmitry
+
+#ifndef MODULES_TASK_3_BRAZHNIK_D_SORT_SHELL_SIMPLE_MERGE_OMP_SHORT_SHELL_TBB_H_
+#define MODULES_TASK_3_BRAZHNIK_D_SORT_SHELL_SIMPLE_MERGE_OMP_SHORT_SHELL_TBB_H_
+#include <utility>
+#include <vector>
+
+std::vector<int> getRandom(int N, int seed = 0);
+std::vector<int> SortShell(const std::vector<int>& a, int size);
+std::vector<std::vector<int>> division(const std::vector<int>& a, unsigned int n);
+std::vector<int> Merge(const std::vector<std::vector<int>>& a, int n, int size);
+std::vector<int> Merge_of_two_vectors(const std::vector<int>& a, const std::vector<int>& b);
+std::vector<int> Shell_with_merge(const std::vector<int>& a, int n, int size);
+std::vector<int> Shell_with_merge_tbb(const std::vector<int>& a, int n, int size);
+
+#endif // MODULES_TASK_3_BRAZHNIK_D_SORT_SHELL_SIMPLE_MERGE_OMP_SHORT_SHELL_TBB_H_
